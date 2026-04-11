29a6f67821172443337946e44231afd01a3aa997
Kyveli-C <KChristoforou@uclan.ac.uk>
2026-04-11 15:45:37 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,56 @@
+//recognition Game
+let gameScore=0;
+document.getElementById("score").innerHTML="Score:"+gameScore;
+
+function correctPicture ()
+{
+    alert ("Well done!");
+    gameScore++;
+    document.getElementById("score").innerHTML="Score:"+gameScore;
+
+}
+
+function wrongPicture ()
+{
+    alert ("Sorry! Try again!");
+}
+
+
+
+//recall Game
+
+let correct=["calculator","cart","basket","shop","wifi","wi-fi","email","mail","webmail","home","house","play","search"];
+
+
+function checkAnswer()
+{
+    let answer=document.getElementById("guess").value;
+    console.log(answer);
+
+    let found=false;
+
+
+    for (let i=0; i<13; i++)
+    {
+        if (answer===correct[i])
+        {
+            found= true;
+            gameScore++;
+            break;
+        }
+    }
+
+    if (found)
+    {
+        alert("Well done!");
+    }
+    else
+    {
+        alert ("Sorry! Try again!");
+    }
+    document.getElementById("score").innerHTML="Score:"+gameScore;
+    console.log("clearing...");
+    document.getElementById("guess").value="";
+}
+
+
